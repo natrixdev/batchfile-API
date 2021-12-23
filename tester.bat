1e23f5894a31954e31e093b412f6a4187c786b7c
natrix_dev <discordlenko@gmail.com>
2021-12-23 22:18:21 +0100

    Create tester.bat

---
+++ tester.bat
@@ -0,0 +1,20 @@
+//change this file to try all the batchs things
+
+
+title Hi Github
+@echo off 
+color a 
+cls
+pause
+cls
+color d
+tile Hi Github 2
+cls
+@echo
+%goto% 
+matrix 
+title hacked 
+cls
+color a 
+cls 
+netstat
